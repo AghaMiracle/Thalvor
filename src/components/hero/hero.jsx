e6bf8447c6a57e37128296dfe206d5c67d922c6f
AghaMiracle <aghamiracle123@gmail.com>
2025-12-31 23:21:25 +0100

    edited hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -8,7 +8,7 @@ const hero = () => {
     <div className='hero container'>
       <div className="hero-text">
         <h1>Miracle Agha</h1>
-        <h3>Gamer - Developer - Cyberscurity (Pen-Tester) - Anything</h3>
+        <h3>Gamer - Developer - Cyberscurity (Pen-Tester)</h3>
         <Link to='programs' smooth={true} offset={0} duration={500} className='btn'> Explore More<img src={right_arrow} alt="My GIF" /></Link>
       </div>
     </div>
